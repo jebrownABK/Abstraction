acb8d9ba4e2d5f5e39984d824a8a263c54e7fe22
Jessica Brown <jebrown@activision.com>
2022-08-17 15:57:28 -0500

    resolving crash issue

--- Abstraction/Intermediate/Build/Win64/AbstractionEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Abstraction/Intermediate/Build/Win64/AbstractionEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.29.30146
-#include "C:/Users/jebrown/OneDrive - Activision Publishing/Documents/Unreal Projects/Abstraction/Abstraction/Intermediate/Build/Win64/AbstractionEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/jebrown/Abstraction/Intermediate/Build/Win64/AbstractionEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
